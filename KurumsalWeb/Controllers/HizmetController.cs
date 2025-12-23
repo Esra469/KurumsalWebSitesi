7bf2c88dc21b65877bb4060d5ee4a106b417d9e7
Esra469 <eesra8352@gmail.com>
2025-12-23 16:59:25 +0300

    update code

---
+++ KurumsalWeb/Controllers/HizmetController.cs
@@ -0,0 +1,59 @@
+﻿using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class HizmetController : Controller
+    {
+        private KurumsalDBContext db = new KurumsalDBContext();
+
+        // GET: Hizmet
+        public ActionResult Index()
+        {
+            return View(db.Hizmet.ToList());
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+       public ActionResult Create(Hizmet hizmet,HttpPostedFileBase ResimURL)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (ResimURL != null)
+                {
+                   
+          
+                    WebImage img = new WebImage(ResimURL.InputStream);
+                    FileInfo imginfo = new FileInfo(ResimURL.FileName);
+
+                    // string logoname = LoginURL.FileName + imginfo.Extension; //masaüstüne nasıl kaydetmişsek o şekilde veritabanına kayıt olacak
+                    string logoname = ResimURL.FileName;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/Hizmet/" + logoname);
+
+                    hizmet.ResimURL = "Uploads/Hizmet/" + logoname;
+
+                }
+
+                db.Hizmet.Add(hizmet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hizmet);
+        }
+    }
+}
