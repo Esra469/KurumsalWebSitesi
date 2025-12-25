424f2f55e5a4dde65e517fbc12ffd3763e8b96c9
Esra469 <eesra8352@gmail.com>
2025-12-25 14:53:54 +0300

    added blog edit controller

--- KurumsalWeb/Controllers/BlogController.cs
+++ KurumsalWeb/Controllers/BlogController.cs
@@ -17,8 +17,11 @@ namespace KurumsalWeb.Controllers
         public ActionResult Index()
 
         {
-           
-            return View(db.Blog.ToList());
+            
+            db.Configuration.LazyLoadingEnabled = false;//Sistemde listeleme yapmak istediğimiz zamn göremiyoruz. listelemeyi yapabilmek için lazyloadingenable kapalı olması gerek bu yüzden false yapıyoruz
+
+            //Burada da üste yazdığımız koddan dolayı Include("kategori tablosunu geleceğini belirtiyoruz.") listeleme iişlemi bu şekilde gerçekleşecektir.
+            return View(db.Blog.Include("Kategori").ToList().OrderByDescending(x=>x.BlogId));
         }
         public ActionResult Create()
         {
@@ -46,6 +49,83 @@ namespace KurumsalWeb.Controllers
             }
             db.Blog.Add(blog);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if(id== null)
+            {
+                return HttpNotFound();
+            }
+            var b = db.Blog.Where(x=>x.BlogId==id).FirstOrDefault();//tek kayıt olacağı için single or Default diye belirttik
+            if(b == null)
+            {
+                return HttpNotFound();
+
+            }
+            ViewBag.KategoriId = new SelectList(db.Kategori, "KategoriId", "KategoriAd", b.KategoriId);
+            return View(b);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id,Blog blog,HttpPostedFileBase ResimURL)
+        {
+            if(ModelState.IsValid)
+            {
+                var b=db.Blog.Where(x=>x.BlogId==id).SingleOrDefault();
+
+
+                if (ResimURL != null)
+                {
+                   
+                    if (!string.IsNullOrEmpty(b.ResimURL))
+                    {
+                        var oldFilePath = Server.MapPath("~/" + b.ResimURL);
+
+                        if (System.IO.File.Exists(oldFilePath))
+                        {
+                            System.IO.File.Delete(oldFilePath);
+                        }
+                    }
+                    WebImage img = new WebImage(ResimURL.InputStream);
+                    FileInfo imginfo = new FileInfo(ResimURL.FileName);
+
+                    // string logoname = LoginURL.FileName + imginfo.Extension; //masaüstüne nasıl kaydetmişsek o şekilde veritabanına kayıt olacak
+                    string blogimgname = ResimURL.FileName;
+                    img.Resize(600, 400);
+                    img.Save("~/Uploads/Blog/" + blogimgname);
+
+                    b.ResimURL = "/Uploads/Blog/" + blogimgname;
+
+                }
+                b.Baslik=blog.Baslik;
+                b.İcerik = blog.İcerik;
+                b.KategoriId= blog.KategoriId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(blog);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var b=db.Blog.Find(id);
+            if(b == null)
+            {
+                return HttpNotFound();
+
+            }
+
+            if (System.IO.File.Exists(Server.MapPath(b.ResimURL)))
+            {
+                System.IO.File.Delete(Server.MapPath(b.ResimURL));
+            }
+            db.Blog.Remove(b);
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
     }
