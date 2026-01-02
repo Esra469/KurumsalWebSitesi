bc0ed6af2b3f20c99c6e90e435e8362b27f723f4
Esra469 <eesra8352@gmail.com>
2026-01-02 15:04:13 +0300

    added new website

--- KurumsalWeb/Controllers/HomeController.cs
+++ KurumsalWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KurumsalWeb.Models.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,29 @@ namespace KurumsalWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private  KurumsalDBContext db=new KurumsalDBContext();
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x => x.HizmetId);
+            ViewBag.İletisim=db.iletisim.SingleOrDefault();
+
+            ViewBag.Blog = db.Blog.OrderByDescending(x => x.BlogId).ToList();
+
             return View();
         }
+        public ActionResult SliderPartial()
+        {
+            var veriler = db.Slider
+                    .OrderByDescending(x => x.SliderId) 
+                    .ToList();
+
+            return View(veriler);//bu şekidle slider tablosu bize gelmiş olur
+        }
+        public ActionResult HizmetPartial()
+        {
+            return View(db.Hizmet.ToList());
+        }
+      
     }
 }
